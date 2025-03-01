95841bc9704dbf63ef995fa2416f37cda48a0972
JosyCouto <josycoutopaixao@gmail.com>
2025-03-01 16:37:28 +0000

    criei scanf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,27 +1,37 @@
 #include <stdio.h>
-typedef struct {
 
-char Estado;
-char Codigo[4];
-char NomeCidade[50];
-int Populacao;
-float Area;
-float PIB;
-int PontosTuristicos;
-}CartasSuperTrunfo;
-int main() {
-    CartasSuperTrunfo cartas[2];
-    printf("\n digite os dados da carta %d:\n, 1");
-    
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+int main(){
+    char Estado;
+    char Codigo;
+    char NomeCidade;
+    int Populacao;
+    float Area;
+    float PIB;
+    int PontoTuristico;
 
-    return 0;
+    printf("Digite o Estado: \n");
+    scanf("%c", &Estado);
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
